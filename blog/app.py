d08229daeb63bca13699f5427c8a460907eac4ed
Artem Burenin <burenotti@gmail.com>
2021-09-22 21:15:36 +0300

    Added routing file

--- blog/app.py
+++ blog/app.py
@@ -1,4 +1,6 @@
 import fastapi
-
+from .routing import global_router
 
 app = fastapi.FastAPI()
+
+app.include_router(global_router)
---
+++ blog/routing.py
@@ -0,0 +1,4 @@
+from fastapi import APIRouter
+
+
+global_router = APIRouter()
