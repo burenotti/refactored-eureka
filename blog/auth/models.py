a586491772898346cb210b11cbd98056b56e66bc
Artem Burenin <burenotti@gmail.com>
2021-09-24 15:21:53 +0300

    Added orm_mode for UserOut

--- blog/auth/models.py
+++ blog/auth/models.py
@@ -2,21 +2,24 @@ from pydantic import BaseModel, Field, EmailStr
 
 
 class BaseUser(BaseModel):
-    username: str = Field(max_length=32)
+	username: str = Field(max_length=32)
 
 
 class UserOut(BaseUser):
-    first_name: str = Field(max_length=64)
-    last_name: str = Field(max_length=64)
-    email: EmailStr = Field()
+	first_name: str = Field(max_length=64)
+	last_name: str = Field(max_length=64)
+	email: EmailStr = Field()
+
+	class Config:
+		orm_mode = True
 
 
 class UserCreate(UserOut):
-    password: str
+	password: str
 
 
 class User(UserOut):
-    password_hash: str
+	password_hash: str
 
 
 class Token(BaseModel):
