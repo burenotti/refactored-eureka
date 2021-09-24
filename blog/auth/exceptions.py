a5d1da2b7e3077f2730086d1901013d110da19a5
Artem Burenin <burenotti@gmail.com>
2021-09-24 15:19:45 +0300

    Added 422 exception

--- blog/auth/exceptions.py
+++ blog/auth/exceptions.py
@@ -1,4 +1,5 @@
 from fastapi import HTTPException, status
+from pydantic import ValidationError
 
 
 class HTTP_401_Exception(HTTPException):
@@ -10,4 +11,14 @@ class HTTP_401_Exception(HTTPException):
 			headers={
 				'WWW-Authenticate': 'Bearer',
 			}
+		)
+
+
+class HTTP_422_Exception(HTTPException):
+
+	def __init__(self, detail: str):
+		super().__init__(
+			status_code=status.HTTP_422_UNPROCESSABLE_ENTITY,
+			detail=detail,
+
 		)
